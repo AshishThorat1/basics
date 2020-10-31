ec4cf906e1bd8166e6776204496b8dd9e68f9553
ADT-ankita <55235936+ADT-ankita@users.noreply.github.com>
2020-10-31 17:17:28 +0530

    Create Looping.cs

---
+++ Looping.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace AllInOneCSharp
+{
+    class Looping
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0;i<=8;i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
